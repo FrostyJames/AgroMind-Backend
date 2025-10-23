3f0a0508b7dbbcd956087390c730855c8b57ff1b
MARSHEL <marshalmburu2@gmail.com>
2025-10-23 11:44:41 +0300

    config

--- app/config.py
+++ app/config.py
@@ -2,13 +2,11 @@ from pydantic_settings import BaseSettings
 from fastapi.security import OAuth2PasswordBearer
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = "supersecretkey"  
+    SECRET_KEY: str
+    DATABASE_URL: str
+    OPENAI_API_KEY: str
 
-    DATABASE_URL: str = "sqlite:///./agromind.db"
-
-    OPENAI_API_KEY: str = ""
-
-    oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+    oauth2_scheme: OAuth2PasswordBearer = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
     class Config:
         env_file = ".env"
