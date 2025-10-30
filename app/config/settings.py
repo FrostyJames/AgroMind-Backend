cba4cf2efb3897e0cc556a238b7a28d6c4aaa559
James Ivan <ivanlavan773@gmail.com>
2025-10-30 03:09:34 +0300

    bug fix:openrouter key

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,10 +1,9 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    OPENAI_API_KEY: str
-    OPENROUTER_API_KEY: str  
     SECRET_KEY: str
     DATABASE_URL: str
+    OPENROUTER_API_KEY: str  # required since it's in .env
 
     class Config:
         env_file = ".env"
